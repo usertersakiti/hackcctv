5a7c9070e284043e4e176e9a64fc03d6b4554a7a
usertersakiti <51637256+usertersakiti@users.noreply.github.com>
2019-06-12 16:57:07 +0700

    Add files via upload

---
+++ cctv.sh
@@ -0,0 +1,181 @@
+#!usr/bin/bash
+#User_Tersakiti
+
+
+
+
+
+
+
+#GITU AJA DI REKODE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+clear
+
+b='\033[34;1m'
+m="\033[95;1m"
+g='\033[32;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+w='\033[37;1m'
+y='\033[33;1m'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo ""
+echo ${b} "<━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━>"
+figlet "Hack-CCTV"
+echo ${g} "<━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━>"
+echo ${y} "[]${g} Ip Cctv Indonesia${r} |${y}Tkj-404 Eror System${g} []"  
+echo ${r} "<━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━>"
+echo ${y} "</>  ${g}Code By   : ${r}User_Tersakiti ${y}   </>"
+echo ${g} "</>  ${r}My Github : ${y}github.com/usertersakiti  ${g}   </>"
+echo ${b} "<━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sleep 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "  ${r}[${w}+${r}] ${w}Silahkan Copy/Pate Alamat Ip Nya !${c}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo  "         ${c}╭╼${r}[${g}1${r}]${c}> ${r}https://114.110.17.6:8895 | MONUMEN NASIONAL"
+sleep 1
+echo  "         ${c}╽ ${r}[${g}2${r}]${c}> ${y}https://114.110.17.6:8903 | HOTEL SULTAN"
+sleep 1
+echo  "         ${c}┣╼${r}[${g}3${r}]${c}> ${g}https://114.110.17.6:8899 | TVRI"
+sleep 1
+echo  "         ${c}╿ ${r}[${g}4${r}]${c}> ${y}https://114.110.17.6:8916 | MERDEKA SELATAN"  
+sleep 1
+echo  "         ${c}╰╼${r}[${g}0${r}]${c}> ${r}https://114.110.17.6:8893 | TAMAN RIA"
+echo ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "  ${r}[${w}+${r}] ${w}Hacking Telah Selesai !${c}"
+
+
